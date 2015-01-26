9b64e3dc9092775ee01b05a4a38063f4b3d2d611
Aaron Klaassen <aaron@outerspacehero.com>
2015-01-26 13:10:36 -0600

    Finish updating datalink spec.

--- spec/ccbill/datalink_spec.rb
+++ spec/ccbill/datalink_spec.rb
@@ -8,14 +8,14 @@ RSpec.describe CCBill::DataLink do
   let(:success_response) do
     double(body:
       [
-        '"EXPIRE","0000","1234567890123456","2013-01-05","2013-01-20","N"',
-        '"EXPIRE","0000","6543210987654321","2013-01-10","2013-01-15","Y"'
+        '"EXPIRE","0000","1234567890123456","2014-01-05","2014-01-20","N"',
+        '"EXPIRE","0000","6543210987654321","2014-01-10","2014-01-15","Y"'
       ].join("\n")
     )
   end
   
-  let(:start_time) { Time.parse("2013-01-01") }
-  let(:end_time)   { Time.parse("2013-01-02") }
+  let(:start_time) { Time.parse("2014-01-01") }
+  let(:end_time)   { Time.parse("2014-01-02") }
   let(:txn_types)  { [:expire] }
   let(:params)     { CCBill::DataLink.new.send(:build_params, start_time, end_time, txn_types) }
 
@@ -46,13 +46,14 @@ RSpec.describe CCBill::DataLink do
       end
 
       it "matches the column names and data correctly" do
-        # response[0][:transaction_type].should    eq "EXPIRE"
-        # response[0][:client_sub_account].should  eq "0000"
-        # response[0][:subscription_id].should     eq "1234567890123456"
-        # response[0][:expire_date].should         eq "2013-01-05"
-        # response[0][:cancel_date].should         eq "2013-01-20"
-        # response[0][:batched_transaction].should eq "N"
-        skip 
+        expect(response[0]).to match({
+          transaction_type:     "EXPIRE",
+          client_sub_account:   "0000",
+          subscription_id:      "1234567890123456",
+          expire_date:          "2014-01-05",
+          cancel_date:          "2014-01-20",
+          batched_transaction:  "N"
+        })
       end
     end
 
